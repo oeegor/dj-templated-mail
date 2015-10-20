e54c8514bdeec1b76016eb469c019eac7f5ad920
Egor Orlov <egor.orlov@ostrovok.ru>
2015-10-20 11:28:08 +0300

    python3 support

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ CHANGES = open(os.path.join(os.path.dirname(__file__), 'CHANGES.md')).read()
 
 setup(
     name='dj-templated-mail',
-    version='0.1.5',
+    version='0.1.6',
     packages=['dj_templated_mail'],
     include_package_data=True,
     license='MIT License',
@@ -45,12 +45,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
